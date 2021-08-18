1bf4f2fbbf0a6be6335e3118d15aba18c14b2127
Colin Rofls <colin@cmyr.net>
2021-08-18 09:45:53 -0400

    Cleanup lexing, no errors in lexing!
    Just report state, and then the parser can handle turning this
    into errors.

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,13 @@ mod lexer;
 
 fn main() {
     let args = Args::get_from_env_or_exit();
-    let contents = fs::read_to_string(&args.path)
-        .expect("file read failed");
-    let (tokens, _) = lexer::tokenize(&contents);
-    eprintln!("{}", lexer::debug_tokens(&tokens));
+    let contents = fs::read_to_string(&args.path).expect("file read failed");
+    let tokens = lexer::tokenize(&contents);
+    for repr in lexer::debug_tokens(&tokens) {
+        println!("{}", repr);
+    }
+    //eprintln!("{}", lexer::debug_tokens(&tokens));
 }
-
-// microbenchmarks to do, one day
-
-//fn is_special_match(byte: u8) -> bool {
-//match byte {
-//b';' | b',' | b'@' | b'\\' | b'-' | b'=' | b'{' | b'}' | b'[' | b']' | b'(' | b')'
-//| b'<' | b'>' | b'\'' => true,
-//_ => false,
-//}
-//}
-
-//fn is_special_ranges(byte: u8) -> bool {
-//(39..=45).contains(&byte)
-//|| (59..=64).contains(&byte)
-//|| (91..=93).contains(&byte)
-//|| byte == 123
-//|| byte == 125
-//}
-
-//fn is_special_bsearch(byte: u8) -> bool {
-//[39, 40, 41, 44, 45, 59, 60, 61, 62, 64, 91, 92, 93, 123, 125]
-//.binary_search(&byte)
-//.is_ok()
-//}
-
-//// could improve this by sorting by frequency
-//fn is_special_linear_scan(byte: u8) -> bool {
-//[39, 40, 41, 44, 45, 59, 60, 61, 62, 64, 91, 92, 93, 123, 125].contains(&byte)
-//}
 
 macro_rules! exit_err {
     ($($arg:tt)*) => ({
