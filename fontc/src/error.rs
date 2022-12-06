28289bda2d7dc56e2fccaada90af8cb485d95203
Cosimo Lupo <clupo@google.com>
2022-12-06 15:22:47 +0000

    replace toml with serde_yaml
    Fixes https://github.com/googlefonts/fontmake-rs/issues/28

--- fontc/src/error.rs
+++ fontc/src/error.rs
@@ -6,8 +6,8 @@ use thiserror::Error;
 pub enum Error {
     #[error("Unrecognized source")]
     UnrecognizedSource(PathBuf),
-    #[error("toml error")]
-    TomlSerError(#[from] toml::ser::Error),
+    #[error("yaml error")]
+    YamlSerError(#[from] serde_yaml::Error),
     #[error("IO failures")]
     IoError(#[from] io::Error),
     #[error("Font IR error")]
