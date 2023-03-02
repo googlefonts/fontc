3d97e4e5c0fc90931862cfc0994b5ef9baa9bf4c
Rod S <rsheeter@google.com>
2023-03-01 23:02:38 -0800

    Compile simple glyphs

--- fontbe/src/error.rs
+++ fontbe/src/error.rs
@@ -1,7 +1,9 @@
-use std::io;
+use std::{fmt::Display, io};
 
 use fea_rs::compile::error::{BinaryCompilationError, CompilerError};
+use fontdrasil::types::GlyphName;
 use thiserror::Error;
+use write_fonts::tables::glyf::BadKurbo;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -11,4 +13,45 @@ pub enum Error {
     FeaAssembleError(#[from] BinaryCompilationError),
     #[error("Fea compilation failure")]
     FeaCompileError(#[from] CompilerError),
+    #[error("'{0}' {1}")]
+    GlyphError(GlyphName, GlyphProblem),
+    #[error("'{0}' {1:?} {2}")]
+    KurboError(GlyphName, BadKurbo, String),
+    #[error("'{glyph}' references {referenced_glyph}, {problem}")]
+    ComponentError {
+        glyph: GlyphName,
+        referenced_glyph: GlyphName,
+        problem: GlyphProblem,
+    },
+    #[error("'{glyph}' {errors:?}")]
+    ComponentErrors {
+        glyph: GlyphName,
+        errors: Vec<Error>,
+    },
+}
+
+#[derive(Debug)]
+pub enum GlyphProblem {
+    InconsistentComponents,
+    InconsistentPathElements,
+    HasComponentsAndPath,
+    MissingDefault,
+    NoComponents,
+    NotInGlyphOrder,
+}
+
+impl Display for GlyphProblem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            GlyphProblem::HasComponentsAndPath => "has components *and* paths",
+            GlyphProblem::InconsistentComponents => {
+                "has different components at different points in designspace"
+            }
+            GlyphProblem::InconsistentPathElements => "has interpolation-incompatible paths",
+            GlyphProblem::MissingDefault => "has no default master",
+            GlyphProblem::NoComponents => "has no components",
+            GlyphProblem::NotInGlyphOrder => "has no entry in glyph order",
+        };
+        f.write_str(message)
+    }
 }
--- fontbe/src/lib.rs
+++ fontbe/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod error;
 pub mod features;
+pub mod glyphs;
 pub mod orchestration;
 pub mod paths;
