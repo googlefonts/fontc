a635b808c3ba59e1a7ff09acdd28d60f9d7b68c1
Rod S <rsheeter@google.com>
2023-03-14 13:44:15 -0700

    An even simpler way to make post, plus update BigEndian to latest def

--- fontbe/src/post.rs
+++ fontbe/src/post.rs
@@ -1,7 +1,7 @@
 //! Generates a [post](https://learn.microsoft.com/en-us/typography/opentype/spec/post) table.
 
 use fontdrasil::orchestration::Work;
-use read_fonts::types::{FWord, Fixed};
+use read_fonts::types::Version16Dot16;
 use write_fonts::tables::post::Post;
 
 use crate::{
@@ -19,16 +19,7 @@ impl Work<Context, Error> for PostWork {
     /// Generate [post](https://learn.microsoft.com/en-us/typography/opentype/spec/post)
     fn exec(&self, context: &Context) -> Result<(), Error> {
         // TODO a more serious post
-        let post = Post::new(
-            Fixed::from_i32(0),
-            FWord::new(0),
-            FWord::new(16),
-            0,
-            0,
-            0,
-            0,
-            0,
-        );
+        let post = Post { version: Version16Dot16::VERSION_3_0, ..Default::default() };
         context.set_post(post);
         Ok(())
     }
