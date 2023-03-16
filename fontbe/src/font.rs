2d4c717245a657fbd05de5979446ac881793965e
Rod <rsheeter@google.com>
2023-03-16 13:47:37 -0700

    Update fontbe/src/font.rs
    Co-authored-by: Colin Rofls <colin@cmyr.net>

--- fontbe/src/font.rs
+++ fontbe/src/font.rs
@@ -30,9 +30,9 @@ impl Work<Context, Error> for FontWork {
         let mut builder = FontBuilder::default();
 
         // A fancier implementation would mmap the files. We basic.
-        for (work_id, tag) in TABLES_TO_MERGE.iter().cloned() {
-            let bytes = context.read_raw(work_id).map_err(Error::IoError)?;
-            builder.add_table(tag, bytes);
+        for (work_id, tag) in TABLES_TO_MERGE {
+            let bytes = context.read_raw(*work_id).map_err(Error::IoError)?;
+            builder.add_table(*tag, bytes);
         }
 
         context.set_font(Bytes::new(builder.build()));
