8d93daa05f117bc971811ca4791450ccc3f04080
Colin Rofls <colin@cmyr.net>
2023-03-17 13:33:12 -0400

    [fontc] Move Config to new 'config' module
    This also makes the 'init' function into a method on this type, but
    otherwise avoids any substantive changes.

--- fontc/src/lib.rs
+++ fontc/src/lib.rs
@@ -1,8 +1,10 @@
 //! A font compiler with aspirations of being fast and safe.
 
 mod args;
+mod config;
 mod error;
 pub mod work;
 
 pub use args::Args;
+pub use config::Config;
 pub use error::Error;
