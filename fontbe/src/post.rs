9fcca3c2ff4a7129c2198b95244511da48241e6f
Cosimo Lupo <clupo@google.com>
2023-03-31 12:05:25 +0100

    build post format 2.0 containing glyph names, by default
    when building TTF fontmake (ufo2ft) by default stores the glyph names in the post table format 2.0, so we should match that behavior. Later we can add an option to drop glyph names and use post format 3.0.
    
    Part of https://github.com/googlefonts/fontmake-rs/issues/146

--- fontbe/src/post.rs
+++ fontbe/src/post.rs
@@ -1,7 +1,6 @@
 //! Generates a [post](https://learn.microsoft.com/en-us/typography/opentype/spec/post) table.
 
 use fontdrasil::orchestration::Work;
-use read_fonts::types::Version16Dot16;
 use write_fonts::tables::post::Post;
 
 use crate::{
@@ -18,11 +17,11 @@ pub fn create_post_work() -> Box<BeWork> {
 impl Work<Context, Error> for PostWork {
     /// Generate [post](https://learn.microsoft.com/en-us/typography/opentype/spec/post)
     fn exec(&self, context: &Context) -> Result<(), Error> {
+        // For now we build a v2 table by default, like fontmake does.
+        // TODO optionally drop glyph names with format 3.0.
         // TODO a more serious post
-        let post = Post {
-            version: Version16Dot16::VERSION_3_0,
-            ..Default::default()
-        };
+        let static_metadata = context.ir.get_final_static_metadata();
+        let post = Post::new_v2(static_metadata.glyph_order.iter().map(|g| g.as_str()));
         context.set_post(post);
         Ok(())
     }
