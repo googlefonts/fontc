90ee041ae978db34f80bd895d6e35bdac33b3805
Rod S <rsheeter@google.com>
2023-05-24 22:26:17 -0700

    Capture the unicode range : bit table, use to populate unicode range

---
+++ resources/scripts/os2_unicode_ranges.py
@@ -0,0 +1,13 @@
+"""Generates ranges to paste into fontbe/src/os2.rs"""
+from fontTools.ttLib.tables.O_S_2f_2 import OS2_UNICODE_RANGES
+
+print("const UNICODE_RANGES: &[(usize, usize, usize)] = &[")
+rust_ranges = []
+for bit, ranges in enumerate(OS2_UNICODE_RANGES):
+    for name, (low, high) in ranges:
+        rust_ranges.append((low, high, bit, name))
+rust_ranges.sort()
+
+for low, high, bit, name in rust_ranges:
+    print(f"    (0x{low:04X}, 0x{high:04X}, {bit}),  // {name}")
+print("];")
