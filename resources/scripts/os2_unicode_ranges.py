e67de0b2a0f8d0e5c5eae7f52222230b3a5021e0
Rod S <rsheeter@google.com>
2023-05-26 10:41:29 -0700

    u32 makes a lot mroe sense

--- resources/scripts/os2_unicode_ranges.py
+++ resources/scripts/os2_unicode_ranges.py
@@ -1,7 +1,7 @@
 """Generates ranges to paste into fontbe/src/os2.rs"""
 from fontTools.ttLib.tables.O_S_2f_2 import OS2_UNICODE_RANGES
 
-print("const UNICODE_RANGES: &[(usize, usize, usize)] = &[")
+print("const UNICODE_RANGES: &[(u32, u32, u32)] = &[")
 rust_ranges = []
 for bit, ranges in enumerate(OS2_UNICODE_RANGES):
     for name, (low, high) in ranges:
