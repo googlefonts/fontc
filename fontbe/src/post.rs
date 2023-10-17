aa77e0e45fac5a67a5dab6dd312054f54c41b38c
Ricky Atkins <ricky.atkins@daltonmaag.com>
2023-10-17 16:25:14 +0100

    Use postscript names when writing post table

--- fontbe/src/post.rs
+++ fontbe/src/post.rs
@@ -37,8 +37,14 @@ impl Work<Context, AnyWorkId, Error> for PostWork {
         // TODO optionally drop glyph names with format 3.0.
         // TODO a more serious post
         let static_metadata = context.ir.static_metadata.get();
+        let postscript_names = &static_metadata.postscript_names;
         let glyph_order = context.ir.glyph_order.get();
-        let mut post = Post::new_v2(glyph_order.iter().map(|g| g.as_str()));
+        let mut post = Post::new_v2(glyph_order.iter().map(|g| {
+            postscript_names
+                .get(g)
+                .map(String::as_str)
+                .unwrap_or(g.as_str())
+        }));
         post.underline_position = FWord::new(static_metadata.misc.underline_position.0 as i16);
         post.underline_thickness = FWord::new(static_metadata.misc.underline_thickness.0 as i16);
         context.post.set_unconditionally(post.into());
