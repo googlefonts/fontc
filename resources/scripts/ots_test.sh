4cce0c0bcb8b90b9baa1ecaf2f1ae16877f07480
Chris Simpkins <chris@sourcefoundry.org>
2023-11-20 17:27:53 -0500

    Bump OTS testing version to latest

--- resources/scripts/ots_test.sh
+++ resources/scripts/ots_test.sh
@@ -7,7 +7,7 @@ cargo run -p fontc -- resources/testdata/static.designspace
 
 cd build
 
-OTS_VER=9.0.0
+OTS_VER=9.1.0
 rm -f "ots-${OTS_VER}-Linux.zip"
 rm -rf "ots-${OTS_VER}-Linux"
 curl -OL "https://github.com/khaledhosny/ots/releases/download/v${OTS_VER}/ots-${OTS_VER}-Linux.zip"
