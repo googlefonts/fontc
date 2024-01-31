72601cfa8d4ea010cccaddc429b505f0696787b0
rsheeter <rsheeter@google.com>
2024-01-31 12:55:48 -0800

    Fix clippy

--- fontc/src/lib.rs
+++ fontc/src/lib.rs
@@ -1047,7 +1047,7 @@ mod tests {
         assert_eq!(1, glyph.contours().len());
         assert_eq!(
             4_usize,
-            glyph.contours().iter().map(|c| c.iter().count()).sum()
+            glyph.contours().iter().map(|c| c.iter().count()).sum::<usize>()
         );
         assert_eq!(
             Bbox {
