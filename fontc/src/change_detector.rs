7392d27c9f2b4e4da2625455a5f8c70be36b1523
rsheeter <rsheeter@google.com>
2024-02-12 13:52:47 -0800

    Make a UFO-only compile run through

--- fontc/src/change_detector.rs
+++ fontc/src/change_detector.rs
@@ -374,6 +374,7 @@ fn ir_source(source: &Path) -> Result<Box<dyn Source>, Error> {
         .ok_or_else(|| Error::UnrecognizedSource(source.to_path_buf()))?;
     match ext {
         "designspace" => Ok(Box::new(DesignSpaceIrSource::new(source.to_path_buf())?)),
+        "ufo" => Ok(Box::new(DesignSpaceIrSource::new(source.to_path_buf())?)),
         "glyphs" => Ok(Box::new(GlyphsIrSource::new(source.to_path_buf()))),
         "glyphspackage" => Ok(Box::new(GlyphsIrSource::new(source.to_path_buf()))),
         "fontra" => Ok(Box::new(FontraIrSource::new(source.to_path_buf())?)),
