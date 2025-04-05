4e04d90fd3e933bf6c536c559cb4471156f01f6a
Harry Dalton <harry@hoolean.com>
2025-04-05 19:39:19 +0100

    Remove unnecessary dependency on vertical glyph metrics

--- fontbe/src/os2.rs
+++ fontbe/src/os2.rs
@@ -525,8 +525,6 @@ impl Work<Context, AnyWorkId, Error> for Os2Work {
             .variant(WorkId::Hmtx)
             .variant(WorkId::Gpos)
             .variant(WorkId::Gsub)
-            .variant(WorkId::Vhea)
-            .variant(WorkId::Vmtx)
             .variant(FeWorkId::ALL_GLYPHS)
             .build()
     }
