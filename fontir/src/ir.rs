7fd240993feb346e835b832bcf5a7e41c045828c
Harry Dalton <harry@hoolean.com>
2025-04-06 19:05:43 +0100

    Tentatively match ufo2ft's height for .notdef

--- fontir/src/ir.rs
+++ fontir/src/ir.rs
@@ -1891,6 +1891,10 @@ impl GlyphBuilder {
         path.line_to((x_min, y_min));
         path.close_path();
 
+        // TODO: Should this be None to match other glyphs' heights? This would deviate from ufo2ft
+        // See https://github.com/googlefonts/ufo2ft/blob/b3895a96/Lib/ufo2ft/outlineCompiler.py#L1656-L1658
+        let height = Some(ascender - descender);
+
         Self {
             name: GlyphName::NOTDEF.clone(),
             emit_to_binary: true,
@@ -1899,6 +1903,7 @@ impl GlyphBuilder {
                 default_location,
                 GlyphInstance {
                     width,
+                    height,
                     contours: vec![path],
                     ..Default::default()
                 },
