eba73cc6570621712993aa2ab0bd7d58e5e52263
Harry Dalton <harry@hoolean.com>
2025-08-03 17:26:48 +0100

    Ensure that unique value is at the default position in tests

--- glyphs2fontir/src/source.rs
+++ glyphs2fontir/src/source.rs
@@ -2508,9 +2508,12 @@ mod tests {
     }
 
     fn unique_value(metrics: &GlobalMetrics, metric: GlobalMetric) -> f64 {
-        let values = metrics.deltas(metric);
-        assert_eq!(1, values.len(), "Too many {metric:?} values: {values:?}");
-        // TODO: Is default region?
+        // Assert that there is no variation, outside of the default being defined.
+        let deltas = metrics.deltas(metric);
+        let [(region, _)] = deltas.as_slice() else {
+            panic!("Too many {metric:?} deltas: {deltas:?}")
+        };
+        assert!(region.is_default());
 
         metrics
             .get(metric, &NormalizedLocation::default())
