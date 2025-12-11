bb1b6bff00f2813fc1133b25ff0a5a21db17cbaf
wmedrano <wmedrano@google.com>
2025-12-11 10:53:30 -0800

    Fix CI

--- docs/performance.md
+++ docs/performance.md
@@ -2,17 +2,22 @@
 
 ## Benchmarking with Criterion
 
-fontc uses [criterion](https://crates.io/crates/criterion) for benchmarking. To run the benchmarks, use the following command:
+
+Only relatively large changes are effectively detected this way: fontc uses
+[criterion](https://crates.io/crates/criterion) for benchmarking. To run the
+benchmarks, use the following command:
 
 ```shell
 cargo bench -p fontc --bench compile
 ```
 
-This will run the benchmarks on a default set of fonts. You can customize the benchmark execution using the following environment variables:
+By default, no benchmarks will be run. You must set `FONTC_BENCH_SRCS` to
+run the benchmarks on your desired set of fonts. You can customize the
+benchmark execution using the following environment variables:
 
 - `FONTC_BENCH_DIR`: Specifies the directory where the font sources are
   located. This path is relative to the `fontc` crate directory. The default
-  value is `../`.
+  value is `../..`.
 - `FONTC_BENCH_SRCS`: A comma-separated list of font sources to compile. For
   example:
   `FONTC_BENCH_SRCS=OswaldFont/sources/Oswald.glyphs,Playfair/sources/Playfair.glyphspackage`
@@ -20,18 +25,37 @@ This will run the benchmarks on a default set of fonts. You can customize the be
 Example usage with custom fonts:
 
 ```shell
-FONTC_BENCH_DIR=/path/to/fonts FONTC_BENCH_SRCS=MyFont/sources/MyFont.glyphs cargo bench -p fontc --bench compile
+export FONTC_BENCH_DIR=../..
+export FONTC_BENCH_SRCS=OswaldFont/sources/Oswald.glyphs,googlesans-flex/sources/GoogleSansFlex.designspace
+cargo bench -p fontc --bench compile
 ```
 
-Criterion generates a report with detailed performance data, which can be found
-in `target/criterion/report/index.html`. This report is useful for comparing the
-performance of different branches or changes.
+```
+    Finished `bench` profile [optimized] target(s) in 0.14s
+     Running benches/compile.rs (target/release/deps/compile-f43e0fcecf20fc96)
+Benchmarking fontc-compile/Oswald.glyphs: Warming up for 3.0000 s
+fontc-compile/Oswald.glyphs
+                        time:   [90.392 ms 92.002 ms 93.544 ms]
+                        change: [−1.2468% +0.8230% +2.7741%] (p = 0.46 > 0.05)
+                        No change in performance detected.
+Benchmarking fontc-compile/GoogleSansFlex.designspace: Warming up for 3.0000 s
+Warning: Unable to complete 10 samples in 5.0s. You may wish to increase target time to 132.4s.
+fontc-compile/GoogleSansFlex.designspace
+                        time:   [12.762 s 12.903 s 13.056 s]
+```
+
+Criterion also generates a report with detailed performance data, which can be
+found in `target/criterion/report/index.html`.
 
 ### Comparing branches
 
-To compare the performance between `main` and your feature branch you can use baselines.
+To compare the performance between `main` and your feature branch you can use
+baselines.
 
 ```shell
+export FONTC_BENCH_DIR=../..
+export FONTC_BENCH_SRCS=OswaldFont/sources/Oswald.glyphs,googlesans-flex/sources/GoogleSansFlex.designspace
+
 # On main, establish a baseline.
 git checkout main
 cargo bench -p fontc --bench compile -- --save-baseline main
@@ -39,6 +63,32 @@ cargo bench -p fontc --bench compile -- --save-baseline main
 # On your feature branch, compare to the baseline
 git checkout your-feature-branch
 cargo bench -p fontc --bench compile -- --baseline main
+```
+
+This outputs readable results to the command line and
+`target/criterion/report/index.html`.
+
+## Comparing branch performance with hyperfine
+
+Similar to Criterion comparisons, but uses the `fontc` cli tool.
+
+```shell
+To compare current branch to main when building Oswald:
+
+$ hyperfine \
+  --parameter-list branch main,$(git rev-parse --abbrev-ref HEAD) \
+  --setup "git checkout {branch} && cargo build --release" \
+   --warmup 5 --runs 250 \
+   --prepare 'rm -rf build/' \
+   "target/release/fontc --source ../OswaldFont/sources/Oswald.glyphs"
+
+...noise...
+
+Summary
+  target/release/fontc --source ../OswaldFont/sources/Oswald.glyphs (branch = nocs) ran
+    1.09 ± 0.09 times faster than target/release/fontc --source ../OswaldFont/sources/Oswald.glyphs (branch = main)
+
+# Yay, it seems to be faster!
 ```
 
 ## Running samply
--- fontc/benches/compile.rs
+++ fontc/benches/compile.rs
@@ -1,10 +1,18 @@
+//! Run benchmarks with `cargo bench -p fontc --bench compile`.
+//!
+//! You can customize the benchmark execution using the following environment variables:
+//! - `FONTC_BENCH_DIR`: Specifies the directory where the font sources are located. Defaults to `../..`.
+//! - `FONTC_BENCH_SRCS`: A comma-separated list of font sources to compile.
+//!
+//! For example: `FONTC_BENCH_DIR=../.. FONTC_BENCH_SRCS=OswaldFont/sources/Oswald.glyphs cargo bench -p fontc --bench compile`
+//!
+//! See `docs/performance.md` for more details on benchmarking and profiling.
+
 use std::borrow::Cow;
 use std::path::Path;
 use std::{hint::black_box, path::PathBuf};
 
-use criterion::{Criterion, criterion_group, criterion_main};
-use fontc;
-use log::error;
+use criterion::{Criterion, SamplingMode, criterion_group, criterion_main};
 
 criterion_group!(benches, compile);
 criterion_main!(benches);
@@ -14,24 +22,23 @@ fn compile(c: &mut Criterion) {
     let mut bench_group = c.benchmark_group("fontc-compile");
     // Criterion requires at least 10 samples. The compile time of fonts is large enough that 10
     // samples is enough to get an idea of acceptable performance.
-    bench_group.sample_size(10);
+    bench_group
+        .sample_size(10)
+        .sampling_mode(SamplingMode::Flat);
 
-    // The default fonts to use. The default should be available for the `benchmark-fontc` job in
-    // .github/workflows/rust.yml.
-    let default_fonts_path =
-        "OswaldFont/sources/Oswald.glyphs,Playfair/sources/Playfair.glyphspackage";
     let source_paths = std::env::var("FONTC_BENCH_SRCS")
         .ok()
         .map(|x| x.to_string())
-        .unwrap_or(default_fonts_path.to_string());
-    for source_path in source_paths.split(',') {
-        let path = &resolve_path(&source_path);
+        .unwrap_or_default();
+    for source_path in source_paths.split(',').filter(|s| !s.is_empty()) {
+        let path = &resolve_path(source_path);
         let bench_name = path.file_name().unwrap().to_string_lossy();
         bench_group.bench_function(bench_name.as_ref(), move |b| {
             b.iter(move || {
-                let source = make_source(path).unwrap();
+                let source = make_source(path);
                 let options = black_box(fontc::Options::default());
-                fontc::generate_font(black_box(source), black_box(options)).unwrap()
+                fontc::generate_font(black_box(source), black_box(options))
+                    .unwrap_or_else(|err| panic!("Failed to run benchmark for {path:?}: {err}"))
             })
         });
     }
@@ -39,30 +46,30 @@ fn compile(c: &mut Criterion) {
 
 fn resolve_path(relative_path: impl AsRef<Path>) -> PathBuf {
     let base_path = std::env::var("FONTC_BENCH_DIR")
-        .map(|x| Cow::Owned(x))
+        .map(Cow::Owned)
         .unwrap_or(Cow::Borrowed("../../"));
     PathBuf::from_iter([Path::new(base_path.as_ref()), relative_path.as_ref()])
 }
 
-fn make_source(path: &Path) -> Result<Box<dyn fontir::source::Source>, fontc::Error> {
-    let input = fontc::Input::new(&path)
-        .inspect_err(|err| match std::env::current_dir() {
-            Ok(current_dir) => {
-                let resolved_path = PathBuf::from_iter([&current_dir, path]);
-                error!(
-                    r#"Failed to open {path:?} with current directory {current_dir:?}
+fn make_source(path: &Path) -> Box<dyn fontir::source::Source> {
+    let input = fontc::Input::new(path).unwrap_or_else(|err| match std::env::current_dir() {
+        Ok(current_dir) => {
+            let resolved_path = PathBuf::from_iter([&current_dir, path]);
+            panic!(
+                r#"Failed to open {path:?} with current directory {current_dir:?}
 Resolved path: {resolved_path:?}
 Err: {err}"#
-                );
-            }
-            Err(wd_err) => {
-                error!(
-                    r#"Failed to get current directory: {wd_err}
+            );
+        }
+        Err(wd_err) => {
+            panic!(
+                r#"Failed to get current directory: {wd_err}
 Failed to open {path:?}
 Err: {err}"#
-                );
-            }
-        })
-        .unwrap();
-    input.create_source()
+            );
+        }
+    });
+    input
+        .create_source()
+        .unwrap_or_else(|err| panic!("Unable to create source for {path:?}: {err}"))
 }
